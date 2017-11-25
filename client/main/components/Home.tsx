f68519d9864bf10136c70896b5dd9157eb2e171b
Alisa Song <alisasong@gmail.com>
2017-11-24 22:32:53 -0800

    Add main Home image and text overlay

--- client/main/components/Home.tsx
+++ client/main/components/Home.tsx
@@ -2,6 +2,13 @@ import { connect } from 'react-redux';
 import * as React from 'react';
 import Header from './Header';
 
+import '../styles/index.css';
+import '../styles/home.css';
+
+const bannerImage = require('../images/home-banner-image.png');
+const logoTransparent = require('../images/logo-transparent.png');
+
+
 interface HomeProps {
 }
 
@@ -11,6 +18,10 @@ class Home extends React.Component<HomeProps> {
             <section>
                 HOME
             <Header />
+            <div className="banner-wrapper">
+                    <img className="banner-image" src={bannerImage} />
+                    <div className="text-overlay">San & Design</div>
+            </div>
             </section>
         );
     }
