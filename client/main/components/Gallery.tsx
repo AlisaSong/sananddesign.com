869468bc24434bde22ce1d9dbf1edf3b72568afc
Alisa Song <alisasong@gmail.com>
2017-11-27 01:25:08 -0800

    Add additional gallery images to Gallery page, update styling

--- client/main/components/Gallery.tsx
+++ client/main/components/Gallery.tsx
@@ -9,6 +9,8 @@ import '../styles/gallery.css';
 const galleryImage1 = require('../images/gallery-1-0.png');
 const galleryImage2 = require('../images/gallery-2-0.png');
 const galleryImage3 = require('../images/gallery-3-0.png');
+const galleryImage4 = require('../images/gallery-4-0.png');
+const galleryImage5 = require('../images/gallery-5-0.png');
 
 interface GalleryProps {
 }
@@ -23,13 +25,21 @@ class Gallery extends React.Component<GalleryProps, any> {
                 image: galleryImage1,
                 link: '/gallery/gallery1'
             }, {
-                    displayText: 'Gallery 2',
-                    image: galleryImage2,
-                    link: '/gallery/gallery2'
+                displayText: 'Gallery 2',
+                image: galleryImage2,
+                link: '/gallery/gallery2'
             }, {
-                    displayText: 'Gallery 3',
-                    image: galleryImage3,
-                    link: '/gallery/gallery3'
+                displayText: 'Gallery 3',
+                image: galleryImage3,
+                link: '/gallery/gallery3'
+            }, {
+                displayText: 'Gallery 4',
+                image: galleryImage4,
+                link: '/gallery/gallery4'
+            }, {
+                displayText: 'Gallery 5',
+                image: galleryImage5,
+                link: '/gallery/gallery5'
             }]
         };
     }
@@ -39,19 +49,21 @@ class Gallery extends React.Component<GalleryProps, any> {
             <section>
                 <Header />
                 <section className="section-wrapper">
-                    <h1>Gallery</h1>
-                    <ul className="galleries">
-                        {this.state.galleries.map((gallery, index) =>
-                            <li className="gallery"
-                                key={index}>
-                                <Link to={gallery.link}>
-                                    <img src={gallery.image} />
-                                    <p className="gallery-title">{gallery.displayText}</p>
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
-                    <Footer />
+                    <section className="gallery-wrapper">
+                        <h1>Gallery</h1>
+                        <ul className="galleries">
+                            {this.state.galleries.map((gallery, index) =>
+                                <li className="gallery"
+                                    key={index}>
+                                    <Link to={gallery.link}>
+                                        <img src={gallery.image} />
+                                        <p className="gallery-title">{gallery.displayText}</p>
+                                    </Link>
+                                </li>
+                            )}
+                        </ul>
+                        <Footer />
+                    </section>
                 </section>
             </section>
         )
