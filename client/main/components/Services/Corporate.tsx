97665242aaac7ef2f7864d8aa7cc88da9a8cb7c1
Alisa Song <alisasong@gmail.com>
2017-12-05 21:02:49 -0800

    Update content in Corporate section

--- client/main/components/Services/Corporate.tsx
+++ client/main/components/Services/Corporate.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
 
+const corporateImage = require('../../images/services-corporate-image.png');
+
 import '../../styles/services.css';
 
 interface CorporateProps {
@@ -22,7 +24,29 @@ class Corporate extends React.Component<CorporateProps, any> {
             <section>
                 <Header />
                 <section className="section-wrapper">
-                    <Footer />
+                    <section className="services-wrapper">
+                        <h1 className="services-title">Corporate Events</h1>
+                        <h2 className="services-quote">Id non, eveniet ut magna nec, nonummy vivamus nonummy dolor facilisis, aenean eros sed,
+                        quis sed modi luctus.</h2>
+                        <img className="services-image" src={corporateImage} />
+                        <h2 className="services-subtitle">Corporate Floral Services</h2>
+                        <p>
+                            Ultricies morbi consectetuer, ullamcorper rutrum urna a vestibulum neque parturient, ante diam mollis id sociis
+                        maecenas pellentesque, vestibulum vel platea aliquam orci. Ante porta dolor sed. Amet nulla. Nulla eget vel est
+                        venenatis vel, wisi et nonummy, ante ipsum ante cras lectus hendrerit condimentum, eu tellus sit, vel dui. Eu
+                        pellentesque nulla dui nec, rutrum suspendisse dui in, id potenti eleifend eleifend sed nec dui. Porta pharetra nec
+                        malesuada, neque cursus nonummy litora tristique, aliquam maecenas, tincidunt convallis eleifend dis, ultricies ut nam.
+                    </p>
+                        <p>
+                            Suspendisse sed justo, fermentum ac eros mi mauris cum. Commodo erat enim commodo pede nullam, nullam sem ac,
+                        convallis montes luctus a porttitor, amet id vehicula nonummy nunc non, commodo proin fusce at. Dolor pulvinar
+                        morbi augue eligendi platea, blandit ante arcu sed tristique, orci velit integer nullam placerat elementum, lacus
+                        semper congue, laoreet praesent in feugiat ante. Morbi nulla, in eros quam, velit varius tincidunt venenatis elit
+                        donec quisque, elit ipsum vel pretium nec. Lectus cursus sed dui quis nibh, faucibus nec curabitur amet suscipit nascetur,
+                        vivamus mattis cursus odit.
+                    </p>
+                        <Footer />
+                    </section>
                 </section>
             </section>
         )
