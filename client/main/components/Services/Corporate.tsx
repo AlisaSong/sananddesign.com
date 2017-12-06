c6891122ab9fbdcece2226a42988c345e30aeab5
Alisa Song <alisasong@gmail.com>
2017-12-06 01:49:53 -0800

    Increase image widths to services subpages, update styling

--- client/main/components/Services/Corporate.tsx
+++ client/main/components/Services/Corporate.tsx
@@ -25,26 +25,23 @@ class Corporate extends React.Component<CorporateProps, any> {
                 <Header />
                 <section className="section-wrapper">
                     <section className="services-wrapper">
-                        <h1 className="services-title">Corporate Events</h1>
-                        <h2 className="services-quote">Id non, eveniet ut magna nec, nonummy vivamus nonummy dolor facilisis, aenean eros sed,
+                        <h1 className="services-title services-text">Corporate Events</h1>
+                        <h2 className="services-quote services-text">Id non, eveniet ut magna nec, nonummy vivamus nonummy dolor facilisis, aenean eros sed,
                         quis sed modi luctus.</h2>
                         <img className="services-image" src={corporateImage} />
-                        <h2 className="services-subtitle">Corporate Floral Services</h2>
-                        <p>
+                        <h2 className="services-subtitle services-text">Corporate Floral Services</h2>
+                        <p className="services-text">
                             Ultricies morbi consectetuer, ullamcorper rutrum urna a vestibulum neque parturient, ante diam mollis id sociis
-                        maecenas pellentesque, vestibulum vel platea aliquam orci. Ante porta dolor sed. Amet nulla. Nulla eget vel est
-                        venenatis vel, wisi et nonummy, ante ipsum ante cras lectus hendrerit condimentum, eu tellus sit, vel dui. Eu
-                        pellentesque nulla dui nec, rutrum suspendisse dui in, id potenti eleifend eleifend sed nec dui. Porta pharetra nec
-                        malesuada, neque cursus nonummy litora tristique, aliquam maecenas, tincidunt convallis eleifend dis, ultricies ut nam.
-                    </p>
-                        <p>
-                            Suspendisse sed justo, fermentum ac eros mi mauris cum. Commodo erat enim commodo pede nullam, nullam sem ac,
-                        convallis montes luctus a porttitor, amet id vehicula nonummy nunc non, commodo proin fusce at. Dolor pulvinar
-                        morbi augue eligendi platea, blandit ante arcu sed tristique, orci velit integer nullam placerat elementum, lacus
-                        semper congue, laoreet praesent in feugiat ante. Morbi nulla, in eros quam, velit varius tincidunt venenatis elit
-                        donec quisque, elit ipsum vel pretium nec. Lectus cursus sed dui quis nibh, faucibus nec curabitur amet suscipit nascetur,
-                        vivamus mattis cursus odit.
-                    </p>
+                        maecenas pellentesque, vestibulum vel platea aliquam orci. Ante porta dolor sed.
+                        </p>
+                        <h2 className="services-subtitle services-text">Menu</h2>
+                        <ul className="services-menu services-text">
+                            <li className="services-menu-item">Ultricies morbi - consectetuer</li>
+                            <li className="services-menu-item">ullamcorper rutrum - urna</li>
+                            <li className="services-menu-item">a vestibulum neque - parturient</li>
+                            <li className="services-menu-item">ante diam mollis - id sociis</li>
+                            <li className="services-menu-item">maecenas pellentesque - vestibulum</li>
+                        </ul>
                         <Footer />
                     </section>
                 </section>
--- client/main/components/Services/RealEstate.tsx
+++ client/main/components/Services/RealEstate.tsx
@@ -25,23 +25,24 @@ class RealEstate extends React.Component<RealEstateProps, any> {
                 <Header />
                 <section className="section-wrapper">
                     <section className="services-wrapper">
-                        <h1 className="services-title">Real Estate Staging</h1>
-                        <h2 className="services-quote">Id non, eveniet ut magna nec, nonummy vivamus nonummy dolor facilisis, aenean eros sed,
+                        <h1 className="services-title services-text">Real Estate Staging</h1>
+                        <h2 className="services-quote services-text">Id non, eveniet ut magna nec, nonummy vivamus nonummy dolor facilisis, aenean eros sed,
                         quis sed modi luctus.</h2>
                         <img className="services-image" src={realEstateImage} />
-                        <h2 className="services-subtitle">Real Estate Staging</h2>
-                        <p>
+                        <h2 className="services-subtitle services-text">Real Estate Staging</h2>
+                        <p className="services-text">
                             Viverra lacus id. Molestie fringilla sed sodales urna feugiat orci, placerat vivamus lacus tincidunt mi justo, sit diam
                         eu pede mauris. Quis nec aliquam. Sed purus curabitur amet sodales neque, a cursus ornare turpis, risus praesent dis
                         vehicula integer, faucibus neque, in euismod.
                         </p>
-                        <p>
-                            Ultricies morbi consectetuer, ullamcorper rutrum urna a vestibulum neque parturient, ante diam mollis id sociis
-                        maecenas pellentesque, vestibulum vel platea aliquam orci. Ante porta dolor sed. Amet nulla. Nulla eget vel est
-                        venenatis vel, wisi et nonummy, ante ipsum ante cras lectus hendrerit condimentum, eu tellus sit, vel dui. Eu
-                        pellentesque nulla dui nec, rutrum suspendisse dui in, id potenti eleifend eleifend sed nec dui. Porta pharetra nec
-                        malesuada, neque cursus nonummy litora tristique, aliquam maecenas, tincidunt convallis eleifend dis, ultricies ut nam.
-                    </p>
+                        <h2 className="services-subtitle services-text">Menu</h2>
+                        <ul className="services-menu services-text">
+                            <li className="services-menu-item">Ultricies morbi - consectetuer</li>
+                            <li className="services-menu-item">ullamcorper rutrum - urna</li>
+                            <li className="services-menu-item">a vestibulum neque - parturient</li>
+                            <li className="services-menu-item">ante diam mollis - id sociis</li>
+                            <li className="services-menu-item">maecenas pellentesque - vestibulum</li>
+                        </ul>
                         <Footer />
                     </section>
                 </section>
