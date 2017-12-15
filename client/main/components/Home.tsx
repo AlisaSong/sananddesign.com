80ae7b4fccdb971624259426394b6142b959ba28
Alisa Song <alisasong@gmail.com>
2017-12-14 19:34:02 -0800

    Update photos in Home pagae

--- client/main/components/Home.tsx
+++ client/main/components/Home.tsx
@@ -9,6 +9,8 @@ const bannerImage = require('../images/home-banner-image.png');
 const imageSmall1 = require('../images/home-image-1.png');
 const imageSmall2 = require('../images/home-image-2.png');
 const imageSmall3 = require('../images/home-image-3.png');
+const imageSmall4 = require('../images/home-image-4.png');
+
 const logoTransparent = require('../images/logo-transparent.png');
 
 
@@ -27,7 +29,9 @@ class Home extends React.Component<HomeProps> {
                 <div className="trio-images-wrapper">
                     <img className="small-image" src={imageSmall1} />
                     <img className="small-image" src={imageSmall2} />
+                    <img className="small-image" src={imageSmall4} />
                     <img className="small-image" src={imageSmall3} />
+
                 </div>
                 <Footer />
             </section>
